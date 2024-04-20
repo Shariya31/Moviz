84e87547ba8494420d3bacbcbb2fb35448b00a62
Shariya31 <abushariya31@gmail.com>
2024-04-20 06:08:37 -0700

    starting the details page on 20 april

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,54 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import './style.scss'
-import useFetch from '../../../hooks/useFetch'
-import Img from '../../../components/lazyLoadImage/Img'
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./style.scss";
+
+import useFetch from "../../../hooks/useFetch";
+
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
-  const [background, setBackground] = useState('')
-  const [query, setQuery] = useState('')
-  const navigate = useNavigate();
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const { url } = useSelector((state) => state.home);
+    const { data, loading } = useFetch("/movie/upcoming");
 
-  const { data, loading } = useFetch('/titles/x/upcoming')
-  console.log('data2', data)
+    useEffect(() => {
+        const bg =
+            url.backdrop +
+            data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg);
+    }, [data]);
 
-  useEffect(() => {
-    const bg = data?.results?.[Math.floor(Math.random() * 10)]?.primaryImage?.url
+    const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
 
-    setBackground(bg)
-  }, [data])
+    return (
+        <div className="heroBanner">
+            {!loading && (
+                <div className="backdrop-img">
+                    <Img src={background} />
+                </div>
+            )}
 
-  const searchQueryHandler = (e) => {
-    if (e.key === 'Enter' && query.length > 0) {
-      navigate(`/search/${query}`)
-    }
-  }
-  return (
-    <div className='heroBanner'>
-      {!loading && <div className="backdrop_img">
-        <Img
-          src={background}
-        />
-      </div>}
-
-      <div className="opacity_layer">
-        
-      </div>
-
-      <ContentWrapper>
-        <div className="heroBannerContent">
-          <span className="title">Welcome</span>
-          <span className="sub_title">Your Fovourite Movies And Tv Shows All At One Place To Discover | Explore Now</span>
-          <div className="searchInput">
-            <input type="text" placeholder='Search Here' onKeyUp={searchQueryHandler} onChange={(e) => setQuery(e.target.value)} value={query} />
-            <button>Search</button>
-          </div>
+            <div className="opacity-layer"></div>
+            <ContentWrapper>
+                <div className="heroBannerContent">
+                    <span className="title">Welcome.</span>
+                    <span className="subTitle">
+                        Millions of movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+                    <div className="searchInput">
+                        <input
+                            type="text"
+                            placeholder="Search for a movie or tv show...."
+                            onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                        />
+                        <button>Search</button>
+                    </div>
+                </div>
+            </ContentWrapper>
         </div>
+    );
+};
 
-      </ContentWrapper>
-    </div>
-  )
-}
-
-export default HeroBanner
+export default HeroBanner;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const  BASE_URL = 'https://moviesdatabase.p.rapidapi.com'
+const  BASE_URL = 'https://api.themoviedb.org/3'
 
 const RAPID_API_KEY = import.meta.env.VITE_APP_RAPID_API_KEY;
 
 const headers = {
-    'X-RapidAPI-Key': RAPID_API_KEY,
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-}
+    Authorization: "Bearer " + RAPID_API_KEY,
+};
 
 export const fetchDataFromApi = async (url, params)=>{
+    
     try {
         const {data} = await axios.get(BASE_URL + url, {
             headers,
